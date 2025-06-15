750b87f7c57d5ea42ced96dd3a3466a6c9bb5433
tsutomu-nagano <copyxshow@gmail.com>
2025-06-16 00:51:55 +0900

    walletのbase64変換のシェル追加

--- wallet2base64.sh
+++ wallet2base64.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # wallet ディレクトリを wallet.zip に圧縮
-zip -r wallet.zip ./worker/python/wallet
+zip -r wallet.zip ./wallet
 
 # base64 エンコードして標準出力
-export WALLET_BASE64=$(base64 wallet.zip)
-# base64 wallet.zip > wallet.b64.txt
+base64 wallet.zip > wallet_b64.txt
 
 # （不要なら zip ファイルを削除）
 rm wallet.zip
