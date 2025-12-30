7d13184508d2ee1dc14e55b495026be4caf44823
tsutomu-nagano <copyxshow@gmail.com>
2025-12-30 17:40:30 +0900

    debug

--- worker/python/oci.py
+++ worker/python/oci.py
@@ -65,6 +65,7 @@ class OCI:
                 with zipfile.ZipFile(temp_wallet_file, "r") as z:
                     z.extractall(".")
 
+        print(self._wallet_dir)
         for f in Path(self._wallet_dir).rglob("*"):
             if f.is_file():
                 print(f)
